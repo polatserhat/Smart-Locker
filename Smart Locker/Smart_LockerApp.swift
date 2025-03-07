56271ccfc182ae41c3943c0bf59b45c892bdbe36
Serhat Polat <132306641+polatserhat@users.noreply.github.com>
2025-03-07 22:39:01 +0300

    Initial Commit

---
+++ Smart Locker/Smart_LockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_LockerApp.swift
+//  Smart Locker
+//
+//  Created by Serhat  on 07.03.25.
+//
+
+import SwiftUI
+
+@main
+struct Smart_LockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
